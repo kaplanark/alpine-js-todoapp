5754378de42d48fc45b4fe9629b52682cd48f626
kaplanark <kaplan.arkan@gmail.com>
2022-05-27 22:58:19 +0300

    Delete request.js

--- request.js
+++
@@ -1,9 +0,0 @@
-export default async function request(action, data = {}) {
-    const formData = new FormData();
-    formData.append('action', 'todos')
-    fetch('http://localhost:5000/todolist/', {
-        method: "POST"
-    })
-        .then(response => response.json())
-        .then(data => this.todos = data)
-}
